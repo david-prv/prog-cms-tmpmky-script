9013e67fb19b81bf8abd0040a9597b10715f8955
david-prv <66866223+david-prv@users.noreply.github.com>
2021-12-08 12:39:27 +0100

    Version 2.0
    [ + ] Remove also open submissions, you want to ignore
    [ # ] Updated closed submission removal
    [ # ] Updated user script information

--- beautify_prog_cms.user.js
+++ beautify_prog_cms.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
-// @name         Prog CMS : Remove Closed Submissions
+// @name         Beautify Prog CMS
 // @namespace    https://david-dewes.de/
 // @version      0.1
-// @description  remove annoying closed submissions
+// @description  remove annoying components
 // @author       David Dewes
 // @match        https://cms.sic.saarland/prog1_21/students/view
 // @icon         https://www.google.com/s2/favicons?domain=sic.saarland
@@ -10,8 +10,10 @@
 // ==/UserScript==
 
 (function() {
-    'use strict';
-    
+
+    const YOUR_TIME_SLOT = "Slot 14:00";
+
+    /* get clean nodes */
     let nodes = document.getElementsByClassName("table-submissions")[0].childNodes[1].childNodes;
     let cleanNodes = [];
     Array.prototype.forEach.call(nodes, function(node) {
@@ -19,7 +21,12 @@
            cleanNodes.push(node);
         }
     });
+
     Array.prototype.forEach.call(cleanNodes, function(cnode) {
+        /* Remove closed submissions */
        if (cnode.childNodes[1].innerHTML.indexOf("Closed") !== -1) cnode.setAttribute('style', 'display:none!important;');
+        /* Remove open submissions that do not belong to you */
+       if (cnode.childNodes[1].innerHTML.indexOf("Open") !== -1 && cnode.childNodes[3].innerHTML.indexOf(YOUR_TIME_SLOT) == -1) cnode.setAttribute('style', 'display:none!important;');
     });
+
 })();
