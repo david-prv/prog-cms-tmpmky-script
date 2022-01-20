f2474a6b1f269468ef67235faffc6ff2911a02a3
david-prv <66866223+david-prv@users.noreply.github.com>
2022-01-20 21:32:45 +0100

    Rollback
    (-) removed redundant features

--- beautify_prog_cms.user.js
+++ beautify_prog_cms.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         Beautify Prog CMS
 // @namespace    https://david-dewes.de/
-// @version      2.3
-// @description  remove annoying components, add problem types
+// @version      2.4
+// @description  remove annoying closed submissions
 // @author       David Dewes
 // @match        https://cms.sic.saarland/prog1_21/students/view
 // @icon         https://www.google.com/s2/favicons?domain=sic.saarland
@@ -11,23 +11,7 @@
 
 (function() {
 
-    const YOUR_TIME_SLOT = "Slot 14:00";
     const CLOSED_TIME_SLOT = "Closed";
-    const OPEN_TIME_SLOT = "Open";
-    const CATCH_ALL_SLOT = "All Slots";
-    const MIDTERM_PROBLEMS = {
-        "Problem 1": "Inversion",
-        "Problem 2": "Fibonacci",
-        "Problem 3": "Reverse",
-        "Problem 4": "Init",
-        "Problem 5": "Decimal",
-        "Problem 6": "Prime",
-        "Problem 7": "Insertionsort",
-        "Problem 8": "Prefix",
-        "Problem 9": "Deeptree",
-        "Problem 10": "Linearization",
-        "Problem 11": "Typing/Evaluation Rules"
-    };
 
     /* fetch clean nodes */
     let nodes = document.getElementsByClassName("table-submissions")[0].childNodes[1].childNodes;
@@ -39,21 +23,7 @@
     });
 
     Array.prototype.forEach.call(cleanNodes, function(cnode) {
-        /* Remove closed submissions */
        if (cnode.childNodes[1].innerHTML.indexOf(CLOSED_TIME_SLOT) !== -1) cnode.setAttribute('style', 'display:none!important;');
-        /* Remove open submissions that do not belong to you */
-       if (cnode.childNodes[1].innerHTML.indexOf(OPEN_TIME_SLOT) !== -1 &&
-           cnode.childNodes[3].innerHTML.indexOf(YOUR_TIME_SLOT) == -1 &&
-           cnode.childNodes[3].innerHTML.indexOf(CATCH_ALL_SLOT) == -1) cnode.setAttribute('style', 'display:none!important;');
     });
-
-    // MIDTERM WISE21/22 PROBLEMS //
-
-    let m_table = document.getElementById("testing_10").childNodes[0].childNodes[0];
-    let m_problem_values = Object.values(MIDTERM_PROBLEMS);
-
-    for(let i = 0; i <= m_problem_values.length; i++) {
-        m_table.innerHTML = m_table.innerHTML.replace(`Problem ${i.toString()}`, `Problem ${i.toString()} (${m_problem_values[i-1]})`);
-    }
 
 })();
