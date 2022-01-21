485530a3f8b603f9dd6aa207431259f768ae6223
david-prv <66866223+david-prv@users.noreply.github.com>
2022-01-21 10:44:19 +0100

    global blur

--- hide_sensitive_data.user.js
+++ hide_sensitive_data.user.js
@@ -11,9 +11,13 @@
 
 (function() {
 
-    let css = ".blurred { color: transparent; text-shadow: 0 0 5px rgb(154 154 154 / 50%); -webkit-transition: text-shadow 0s; -moz-transition: text-shadow 0s; } .blurred:hover {color: transparent; text-shadow: 0 0 0 rgba(255,255,255,1); -webkit-transition: text-shadow 0.1s; -moz-transition: text-shadow 0.1s;} ",
+    const BLUR_GLOBAL = true;
+
+    let css = ".blurred { color: transparent; text-shadow: 0 0 5px rgb(154 154 154 / 50%); -webkit-transition: text-shadow 0s; -moz-transition: text-shadow 0s; } .blurred:hover {color: inherit; text-shadow: 0 0 0 rgba(255,255,255,1); -webkit-transition: text-shadow 0.1s; -moz-transition: text-shadow 0.1s;} ",
         styleObj = document.createElement("style"),
-        head = document.head || document.getElementsByTagName('head')[0];
+        head = document.head || document.getElementsByTagName('head')[0],
+        matriculation,
+        fullname;
     head.appendChild(styleObj);
     styleObj.type = 'text/css';
     if (styleObj.styleSheet){ styleObj.styleSheet.cssText = css; } else { styleObj.appendChild(document.createTextNode(css)); }
@@ -27,9 +31,15 @@
         }
     });
 
+    fullname = cleanNodes[0].innerHTML;
+    matriculation = cleanNodes[2].innerHTML;
+
     /* Blur */
     Array.prototype.forEach.call(cleanNodes, function(cnode) {
        cnode.classList.add("blurred");
     });
+
+    // global
+    if(BLUR_GLOBAL) document.body.innerHTML = document.body.innerHTML.replaceAll(matriculation, "<span class='blurred'>" + matriculation + "</span>").replaceAll(fullname, "<span class='blurred'>" + fullname + "</span>");
 
 })();
